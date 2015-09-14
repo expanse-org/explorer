69109cd9ddf054f767e59ab45bb0015ef594cda2
Christopher Franko <chrisjfranko@gmail.com>
2015-09-14 09:26:28 -0400

    update app.js

--- app/app.js
+++ app/app.js
@@ -27,7 +27,7 @@ angular.module('ethExplorer', ['ngRoute','ui.bootstrap'])
     }])
     .run(function($rootScope) {
         var web3 = require('web3');
-        web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
+        web3.setProvider(new web3.providers.HttpProvider("http://localhost:9656"));
         $rootScope.web3=web3;
         function sleepFor( sleepDuration ){
             var now = new Date().getTime();
